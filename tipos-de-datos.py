2d17f79517b30587762c3963b10cad900dc4bcb9
danielchamo027 <danielchamo025@gmail.com>
2024-08-17 17:58:51 -0500

    Update tipos-de-datos.py

---
+++ tipos-de-datos.py
@@ -0,0 +1,14 @@
+"string" # se utiliza para tipod de datos de texto
+'string'
+"""string"""
+'''string'''
+""" TIS DATOS SON 
+NOMBRE: DANIEL
+APELLIDO: CHAMORRO"""
+#tipos de datos numericos 
+4#tipos de datos entero
+4.5 #tipode datos flotante 
+
+#tipos de datos booleanos
+True#True va T mayuscula
+False#False va con F mayuscula
