4052d8cc3a6ec98142f1dc4317f3d70463fb5710
DANIEL CHAMORRO <danielchamo025@gmail.com>
2024-08-17 18:46:23 -0500

    avanzamos variables

---
+++ variables/variables.py
@@ -0,0 +1,13 @@
+nombre = "Daniel"
+bienvenida = f"hola  {nombre} feliz noche "
+#del bienvenida 
+print(bienvenida) 
+
+print("ola" in bienvenida)
+print("hola" not in bienvenida)
+
+#esto es CamelCase
+nombrecompletodetuprofe = "Jheyson Galvis"
+
+#SnakeCase es la que debemos usar en python
+nombre_completo_de_tu_profe = "jheyson Galvis"
